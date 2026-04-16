bd8c8ec44b36c7e4a599e96db52eddafef8c3d22
gws <gws@ea.se>
2026-04-16 15:18:05 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Cross build rsyslog for aarch64 ";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    crossPkgs = import nixpkgs {
+      system = "x86_64-linux";
+      config.allowUnfree = true;
+      crossSystem = {config = "aarch64-unknown-linux-gnu";};
+    };
+  in {
+    packages.x86_64-linux.crossAarch64 = {
+      rsyslog = crossPkgs.callPackage ./rsyslog.nix {};
+    };
+  };
+}
